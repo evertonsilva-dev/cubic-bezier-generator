3c5e4f57a46ad64acae346d207ee4c90f62d0838
Everton <evertonsilvadev@gmail.com>
2020-08-29 21:03:34 -0300

    corrigindo coordenadas

--- src/components/BezierController/index.tsx
+++ src/components/BezierController/index.tsx
@@ -8,19 +8,19 @@ import { AnimationBox, AnimationController, AnimationItem, Bezier, BezierContain
 export default function BezierController() {
   interface iBezier {
     x1: number;
+    y1: number;
     x2: number;
-    x3: number;
-    x4: number;
+    y2: number;
   }
-  const [bezier, setBezier] = useState<iBezier>({ x1: 0, x2: 0, x3: 0, x4: 0 });
+  const [bezier, setBezier] = useState<iBezier>({ x1: 0, y1: 0, x2: 0, y2: 0 });
 
   return (
     <BezierContainer>
       <AnimationBox>
-        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.x2}, ${bezier.x3}, ${bezier.x4})`} />
-        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.x2}, ${bezier.x3}, ${bezier.x4})`} />
-        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.x2}, ${bezier.x3}, ${bezier.x4})`} />
-        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.x2}, ${bezier.x3}, ${bezier.x4})`} />
+        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.y1}, ${bezier.x2}, ${bezier.y2})`} />
+        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.y1}, ${bezier.x2}, ${bezier.y2})`} />
+        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.y1}, ${bezier.x2}, ${bezier.y2})`} />
+        <AnimationItem title={`cubic-bezier(${bezier.x1}, ${bezier.y1}, ${bezier.x2}, ${bezier.y2})`} />
       </AnimationBox>
       <AnimationController>
         <Subtitle>Animation Controller:</Subtitle>
@@ -37,6 +37,19 @@ export default function BezierController() {
           />
         </div>
         <div className="cont-input">
+          <label htmlFor="y1">y1: {bezier.y1}</label>
+          <Slider
+            defaultValue={bezier.y1}
+            ValueLabelComponent={ValueLabelComponent}
+            aria-label="custom thumb label"
+            id="y1"
+            onChange={(e, val) => {
+              setBezier({ ...bezier, y1: Number(val) / 100 });
+            }}
+          />
+        </div>
+
+        <div className="cont-input">
           <label htmlFor="x2">x2: {bezier.x2}</label>
           <Slider
             defaultValue={bezier.x2}
@@ -48,28 +61,15 @@ export default function BezierController() {
             }}
           />
         </div>
-
         <div className="cont-input">
-          <label htmlFor="x3">x3: {bezier.x3}</label>
+          <label htmlFor="y2">y2: {bezier.y2}</label>
           <Slider
-            defaultValue={bezier.x3}
+            defaultValue={bezier.y2}
             ValueLabelComponent={ValueLabelComponent}
             aria-label="custom thumb label"
-            id="x3"
+            id="y2"
             onChange={(e, val) => {
-              setBezier({ ...bezier, x3: Number(val) / 100 });
-            }}
-          />
-        </div>
-        <div className="cont-input">
-          <label htmlFor="x4">x4: {bezier.x4}</label>
-          <Slider
-            defaultValue={bezier.x4}
-            ValueLabelComponent={ValueLabelComponent}
-            aria-label="custom thumb label"
-            id="x4"
-            onChange={(e, val) => {
-              setBezier({ ...bezier, x4: Number(val) / 100 });
+              setBezier({ ...bezier, y2: Number(val) / 100 });
             }}
           />
         </div>
@@ -87,7 +87,7 @@ export default function BezierController() {
             type="text"
             id="bezier"
             readOnly
-            value={`cubic-bezier(${bezier.x1},${bezier.x2},${bezier.x3},${bezier.x4})`}
+            value={`cubic-bezier(${bezier.x1},${bezier.y1},${bezier.x2},${bezier.y2})`}
           />
         </Bezier>
       </AnimationController>
